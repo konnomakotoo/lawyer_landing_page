cc36cbe4f3b64908fa29d54a359921dd38c167ea
Кермен Мамышева <kermen@MacBook-Pro-Kermen.local>
2025-06-13 16:16:12 +0300

    pulled feature/dev

--- server/db/database.js
+++ server/db/database.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 module.exports = {
 development: {
-username: process.env.DB_USER || 'postgres',
-password: process.env.DB_PASS || 'postgres',
-database: process.env.DB_NAME || 'dbName',
+username: process.env.DB_USER,
+password: process.env.DB_PASS,
+database: process.env.DB_NAME,
 host: process.env.DB_HOST || '127.0.0.1',
 dialect: 'postgres',
 },
 test: {
-username: process.env.DB_USER || 'postgres',
-password: process.env.DB_PASS || 'postgres',
-database: process.env.DB_NAME_TEST || 'dbName_test',
+username: process.env.DB_USER,
+password: process.env.DB_PASS,
+database: process.env.DB_NAME_TEST,
 host: process.env.DB_HOST || '127.0.0.1',
 dialect: 'postgres',
 },
